e1d94e2e7bbab6a6c0a98d54a6dd3868d7bbe07e
Konstantin Rosenberg <konsi.rs@web.de>
2020-12-28 17:27:21 +0100

    added links for different testing pages

--- Experiment/app.js
+++ Experiment/app.js
@@ -5,8 +5,33 @@ var path = require('path');
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index_normal.html'));
+    res.sendFile(path.join(__dirname + '/html_pages/index_normal.html'));
 });
+
+app.get('/async', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html_pages/async_css.html'));
+});
+
+app.get('/criticalHead', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html_pages/criticalHead.html'));
+});
+
+app.get('/criticalStyles', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html_pages/criticalStyles.html'));
+});
+
+app.get('/cSSSprites', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html_pages/CSSSprites.html'));
+});
+
+app.get('/importInCSS', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html_pages/importInCSS.html'));
+});
+
+app.get('/inlineCSS', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html_pages/inlineCSS.html'));
+});
+
 
 app.listen(8080,()=>{
     console.log("app is listening on port 8080");
