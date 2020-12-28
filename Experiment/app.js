2e435b772eb904140e88a36aa118b2b05494bcaa
Ravell Heerdegen <rh075@hdm-stuttgart.de>
2020-12-28 16:22:01 +0100

    Nodemon und Standardseite fertig

--- Experiment/app.js
+++ Experiment/app.js
@@ -5,7 +5,7 @@ var path = require('path');
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/index_normal.html'));
 });
 
 app.listen(8080,()=>{
